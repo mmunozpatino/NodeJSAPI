38c1e1444e2b7d2155f90e63bb28edcdeacd9184
Mercedes <mercedesmp1305@gmail.com>
2017-07-05 23:21:51 -0300

    middleware agregado

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var express = require('express'); //call express
 var app = express();
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o');//connect to our database
+
+var Bear = require('./app/models/bear');
 //configure app to use bodyParser() this will let us get the data from a POST
 
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -15,14 +19,23 @@ var port = process.env.PORT || 8080; //set our port
 
 var router = express.Router();
 
-//test route to make sure everything ir working
+// el router se ejecuta en el orden que se defina acá
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+//test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 
 router.get('/', function(req, res) {
-   res.json({ message: 'hooray! welcome to our api"'});
+   res.json({ message: 'hooray! welcome to our api!'});
 });
 
 //more toutes for out API will happen here
 
+//resitrer our routes
 app.use('/api', router);
 
 //start the server
